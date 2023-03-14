b1a09f1ac2cd2d64d9b2b7b722ea41bbef2f04ff
Asbjorn Bordoy <xbordoy@stuba.sk>
2023-03-14 11:18:34 +0100

    Uploading changes

--- DatastructuresAndAlgorithms/src/AVLTreeHelper.java
+++ DatastructuresAndAlgorithms/src/AVLTreeHelper.java
@@ -2,11 +2,14 @@
 import java.util.Random;
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class AVLTreeHelper {
-    private static final String CSV_FILENAME = "insert_benchmark.csv";
+    private static final String InsertBenchmarkCSV = "insert_benchmark.csv";
+    private static final String SearchBenchmarkCSV = "search_benchmark.csv";
     static Scanner scanner = new Scanner(System.in);
     static AVLTree avlTree = new AVLTree();
 
@@ -15,9 +18,10 @@ public class AVLTreeHelper {
         do {
             System.out.println("\nAVLTree Operations\n");
             System.out.println("1. insert ");
-            System.out.println("2. insert_benchmark ");
+            System.out.println("2. insert_benchmark");
             System.out.println("3. search");
-            System.out.println("4. count nodes");
+            System.out.println("4. search_benchmark");
+            System.out.println("5. count nodes");
             int choice = scanner.nextInt();
             switch (choice) {
                 case 1:
@@ -34,7 +38,13 @@ public class AVLTreeHelper {
                     System.out.println("Enter integer element to search");
                     System.out.println("Search result : " + avlTree.search(scanner.nextInt()));
                     break;
+
                 case 4:
+                    System.out.println("Enter integer element to insert");
+                    search_benchmark(scanner.nextInt());
+                    break;
+
+                case 5:
                     System.out.println("Nodes = " + avlTree.countNodes());
                     break;
                 default:
@@ -50,14 +60,41 @@ public class AVLTreeHelper {
         } while (ch == 'Y' || ch == 'y');
     }
 
+    private static int[] readCSVFile(String csvFilename, int NumberOfSearch) {
+        int[] numbers = new int[NumberOfSearch];
+        int index = 0;
+        int num = 0;
+    
+        try (BufferedReader br = new BufferedReader(new FileReader(csvFilename))) {
+            String line;
+            while ((line = br.readLine()) != null && index < NumberOfSearch) {
+                String[] data = line.split(",");
+                
+                if (data[0].matches("-?\\d+(\\.\\d+)?")) {
+                    num = Integer.parseInt(data[0].trim());
+                    numbers[index++] = num;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    
+        return numbers;
+    }
+    
 
 
-    private static void insert_benchmark(int numInserts) {
-        long startTime, endTime, elapsedTime;
+
+    /**
+     * @param js
+     * @param numInserts
+     */
+    private static void search_benchmark(int numSearches) {
+        long startTime, elapsedTime;
         Random random = new Random();
         ArrayList<Integer> list = new ArrayList<Integer>();
         try {
-            FileWriter writer = new FileWriter(CSV_FILENAME);
+            FileWriter writer = new FileWriter(SearchBenchmarkCSV);
 
             writer.append("Number of Inserts");
             writer.append(",");
@@ -66,9 +103,9 @@ public class AVLTreeHelper {
 
             // perform insert operations and measure time taken
             startTime = System.nanoTime();
-            for (int i = 0; i < numInserts; i++) {
+            for (int i = 0; i < numSearches; i++) {
                 int value = random.nextInt();
-                avlTree.insert(numInserts);
+                avlTree.search(value);
 
 
                 elapsedTime = (System.nanoTime() - startTime) / 1000000;
@@ -79,7 +116,7 @@ public class AVLTreeHelper {
                 writer.append("\n");
 
                 // print results
-                System.out.println("Time taken for " + numInserts + " insert operations: " + elapsedTime + " nanoseconds");
+                //System.out.println("Time taken for " + value + " insert operations: " + elapsedTime + " nanoseconds");
             }
 
             writer.flush();
@@ -88,5 +125,46 @@ public class AVLTreeHelper {
             e.printStackTrace();
         }
     }
+
+
+
+    private static void insert_benchmark(int numInserts) {
+        long startTime, endTime, elapsedTime;
+        Random random = new Random();
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        try {
+            FileWriter writer = new FileWriter(InsertBenchmarkCSV);
+
+            writer.append("Number of Inserts");
+            writer.append(",");
+            writer.append("Elapsed Time (ms)");
+            writer.append("\n");
+
+            // perform insert operations and measure time taken
+            startTime = System.nanoTime();
+            for (int i = 0; i < numInserts; i++) {
+                int value = random.nextInt();
+                avlTree.insert(value);
+
+
+                elapsedTime = (System.nanoTime() - startTime) / 1000000;
+
+                writer.append(Integer.toString(i));
+                writer.append(",");
+                writer.append(Long.toString(elapsedTime));
+                writer.append("\n");
+
+                // print results
+                //System.out.println("Time taken for " + value + " insert operations: " + elapsedTime + " nanoseconds");
+            }
+
+            writer.flush();
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 
 }
